d7bad4b29596816327b46ba2c867e5036539b868
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-01-05 10:41:02 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+//! Dependencies
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//? Pages
+import Nav from './Components/Nav';
+import Home from './Pages/Home';
+import Show from './Pages/Show';
+import Index from './Pages/Index';
+import New from './Pages/New';
+import Edit from './Pages/Edit';
+import Error from './Pages/Error';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Pursuit App Template</h1>
+      <Router>
+        <Nav />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/transactions" element={<Index />} />
+            <Route path="/transactions/new" element={<New />} />
+            <Route path="/transactions/:index" element={<Show />} />
+            <Route path="/transactions/:index/edit" element={<Edit />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
